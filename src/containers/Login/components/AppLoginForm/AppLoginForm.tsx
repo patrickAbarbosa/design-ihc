099284187a54d1205d46df621b5a65b0bc21df68
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2022-01-30 22:06:20 -0300

    feat: add example login

--- src/containers/Login/components/AppLoginForm/AppLoginForm.tsx
+++ src/containers/Login/components/AppLoginForm/AppLoginForm.tsx
@@ -1,23 +1,76 @@
-import { Flex, FormControl } from '@chakra-ui/react'
-import React from 'react'
-import { AppButton } from '../../../../components/AppButton'
-import { AppInput } from '../../../../components/AppInput'
-import { AppText } from '../../../../components/AppText'
+import { Box, Flex, FormControl, useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import { AppButton } from "../../../../components/AppButton";
+import { AppInput } from "../../../../components/AppInput";
+import { AppText } from "../../../../components/AppText";
+import { useHistory } from "react-router-dom";
 
 export const AppLoginForm = () => {
-    return (
-        <Flex w="310px" flexDir="column" boxShadow="0px 0px 14px rgba(0, 0, 0, 0.1)" rounded="30px" px="18px" pt="22px" pb="8px">
-            <AppText variant="heading-md">Login</AppText>
+  const toast = useToast();
+  const history = useHistory();
+  const [cpf, setCPF] = useState("");
+  const [password, setPassword] = useState("");
 
-            <FormControl>
-                <AppInput placeholder="CPF" type="tel" mb={4} />
+  const handleCPF = (text: string) => {
+    setCPF(text.replace(/\D/g, ""));
+  };
 
-                <AppInput placeholder="Senha" type="password" mb={4} />
+  const handlePassword = (text: string) => {
+    setPassword(text);
+  };
 
-                <AppButton w="full">ENTRAR</AppButton>
-            </FormControl>
+  const handleSubmit = () => {
+    if (cpf === "29797751007" && password === "1234") {
+      history.push("/minhas-reservas");
+    } else if (cpf === "18357163009" && password === "admin") {
+      history.push("/recursos");
+    } else {
+      toast({
+        position: "bottom",
+        render: () => (
+          <Box color="white" rounded="50px" p={3} bg="error">
+            Usuário inválido!
+          </Box>
+        ),
+      });
+    }
+  };
 
-            <AppButton mx="auto" variant="link-default">Esqueci minha senha</AppButton>
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      w="310px"
+      flexDir="column"
+      boxShadow="0px 0px 14px rgba(0, 0, 0, 0.1)"
+      rounded="30px"
+      px="18px"
+      pt="22px"
+      pb="8px"
+    >
+      <AppText variant="heading-md">Login</AppText>
+
+      <FormControl>
+        <AppInput
+          placeholder="CPF"
+          type="tel"
+          mb={4}
+          onChange={(e) => handleCPF(e.target.value)}
+        />
+
+        <AppInput
+          placeholder="Senha"
+          type="password"
+          mb={4}
+          onChange={(e) => handlePassword(e.target.value)}
+        />
+
+        <AppButton w="full" onClick={handleSubmit}>
+          ENTRAR
+        </AppButton>
+      </FormControl>
+
+      <AppButton mx="auto" variant="link-default">
+        Esqueci minha senha
+      </AppButton>
+    </Flex>
+  );
+};
